b3aa15e5eb4dce25ac9148e2f9f5183636fdfbd6
Dinos Kanakakis <kkanakakis@iknowhow.com>
2022-11-21 17:05:28 +0200

    wip: SecurityConfig

--- src/main/java/com/project/dinopedia/configuration/SecurityConfig.java
+++ src/main/java/com/project/dinopedia/configuration/SecurityConfig.java
@@ -1,17 +1,17 @@
 package com.project.dinopedia.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, proxyTargetClass = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfig {
 //    @Autowired
 //    private CustomAuthenticationProvider customAuthenticationProvider;
 
@@ -20,11 +20,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        auth.authenticationProvider(customAuthenticationProvider);
 //    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeRequests().anyRequest().authenticated();
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic().and()
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and().authorizeRequests().anyRequest().authenticated();
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/products/**")
+                .permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/dinosaur")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
     }
 }
